49b2110ccb91f61e792e74065f4a227a92f10dcb
prahladinala <prahlad2018@gmail.com>
2021-04-15 21:42:12 +0530

    Added Files with Build Folder

---
+++ build/precache-manifest.6e9009ff8cc41014fc136629f55380ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f17123ea9db0792186bfbe7ec990c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e1002ceb560354fbcc9",
+    "url": "./static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "./static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1002ceb560354fbcc9",
+    "url": "./static/js/main.e016b0f2.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  }
+]);
